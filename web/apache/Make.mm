9c17e0747823dd041c69f5ccc620ada1779f322a
Michael Aivazis <michael.aivazis@orthologue.com>
2014-09-05 12:23:28 +0300

    web/apache: added apache 2.4 configuration file

---
+++ web/apache/Make.mm
@@ -0,0 +1,21 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# orthologue
+# (c) 1998-2014 all rights reserved
+#
+
+
+PROJECT = config
+PACKAGE = apache
+
+
+WWW_SERVER=root@orthologue.com
+
+all: tidy
+
+deploy:
+	ssh $(WWW_SERVER) 'service apache2 restart'
+
+
+# end of file 
