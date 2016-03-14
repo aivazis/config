156495fca41c633234382f62a22aecc973713fc5
Michael Aivazis <michael.aivazis@orthologue.com>
2016-03-13 21:11:49 -0700

    people: automated the deployment of the authorized keys; updated alec's key

--- people/Make.mm
+++ people/Make.mm
@@ -2,15 +2,25 @@
 #
 # michael a.g. aïvázis
 # orthologue
-# (c) 1998-2013 all rights reserved
+# (c) 1998-2016 all rights reserved
 #
 
+# add this to the clean pile
+PROJ_CLEAN += authorized_keys
+PROJ_LIVE_ADMIN = root
+PROJ_LIVE_HOST = orthologue.com
+PROJ_LIVE_HOME = /home/projects/config
 
-PROJECT = pyre
-PACKAGE = people
+# the list of public keys
+PUBLIC_KEYS = $(wildcard *.pub)
 
-
+# standard targets
 all: tidy
+# make the autorized keys file
+authorized_keys: $(PUBLIC_KEYS) grant.py grant.pfg Make.mm
+	./grant.py
 
+live: authorized_keys
+	$(SCP) $< $(PROJ_LIVE_ADMIN)@$(PROJ_LIVE_HOST):$(PROJ_LIVE_HOME)/.ssh
 
-# end of file 
+# end of file
--- people/grant.py
+++ people/grant.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # michael a.g. aïvázis
-# orhtologue
-# (c) 1998-2013 all rights reserved
+# orthologue
+# (c) 1998-2016 all rights reserved
 #
 
 
@@ -11,60 +11,80 @@
 Build the {authorized_keys} file that grants write access to the repository
 """
 
+
 # externals
 import os
-# settings
-roll = '.'
-home = '/home/projects/config'
-root = os.path.join(home, 'repository')
+# framework access
+import pyre
 
 
-# build the {authorized_keys} file
-def grant():
+# the app declaration
+class Grant(pyre.application):
     """
-    Build the {authorized_keys} file
+    Build the {authorized_keys} file that grants write access to the repository
     """
-    # create the output file
-    authorized = open('authorized_keys', 'w')
-    # the command template
-    command = (
-            'command="bzr serve --inet --directory={} --allow-writes"'.format(root) +
+
+
+    # public state
+    repository = pyre.properties.str()
+    admin = pyre.properties.list(schema=pyre.properties.str())
+    readers = pyre.properties.list(schema=pyre.properties.str())
+    writers = pyre.properties.list(schema=pyre.properties.str())
+
+
+    @pyre.export
+    def main(self, *args, **kwds):
+        """
+        Build the {authorized_keys} file
+        """
+        # the command template
+        repositoryAccess = (
+            'command="bzr serve --inet --directory={}{{}}"'.format(self.repository) +
             ',no-port-forwarding,no-X11-forwarding,no-agent-forwarding {}'
             )
-    # add the user keys to the file
-    authorized.writelines(command.format(key) for user,key in readKeys())
-    # close the file
-    authorized.close()
-    # all done
-    return
+
+        # create the output file
+        with open('authorized_keys', 'w') as authorized:
+            # team members with login privileges
+            for user in self.admin:
+                # copy the keys
+                authorized.writelines(self.readKeys(user))
+            # team members with write access to the repository
+            for user in self.writers:
+                # build the line and add write privileges
+                authorized.writelines(
+                    repositoryAccess.format(' --allow-writes', key) for key in self.readKeys(user))
+            # team members with read-only access
+            for user in self.readers:
+                # build the line without write privileges
+                authorized.writelines(
+                    repositoryAccess.format('', key) for key in self.readKeys(user))
+
+        # all done
+        return 0
 
 
-# read the keys
-def readKeys():
-    """
-    Open all files with the extension {.pub} in the current directory and return all the keys
-    they contain
-    """
-    # for each file in the roll directory
-    for filename in os.listdir(roll):
-        # extract the name of the user and the extension of the file
-        user, ext = os.path.splitext(filename)
-        # skip files that do not contain public keys
-        if ext != '.pub': continue
+    # implementation details
+    def readKeys(self, user):
+        """
+        Open all the public key file for the given {user} and return all the keys it contains
+        """
         # assemble the path to the key file
-        keyfile = os.path.abspath(os.path.join(roll, filename))
+        keyfile = user + '.pub'
         # read the contents
         for key in open(keyfile, 'r'):
             # show me the key
-            yield user, key
-    # all done
-    return
+            yield key
+        # all done
+        return
 
 
 # main
 if __name__ == "__main__":
-    # do...
-    grant()
+    # create one
+    grant = Grant(name='grant')
+    # do
+    grant.run()
 
 
-# end of file 
+# end of file
