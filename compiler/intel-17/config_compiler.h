5465b7fd7603cb4da3dab7c7e977af9afed6deba
Michael Aivazis <michael.aivazis@orthologue.com>
2016-09-27 10:28:54 +0300

    compiler: added support for {intel-17}; note the transition to lower case for the compiler name

---
+++ compiler/intel-17/config_compiler.h
@@ -0,0 +1,18 @@
+/*
+// -*- C -*-
+//
+// michael a.g. aïvázis
+// orthologue
+// (c) 1998-2016 all rights reserved
+//
+*/
+
+#if !defined(__config_compiler_h__)
+#define __config_compiler_h__
+
+#include <config_f77.h>
+
+#endif
+
+
+/* end of file */
