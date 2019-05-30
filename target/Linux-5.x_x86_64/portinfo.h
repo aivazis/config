3a5f3d6e710b6fde0cfa484fdea0155817c97976
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-05-30 00:05:01 +0000

    added support for {Linux-5.x}

---
+++ target/Linux-5.x_x86_64/portinfo.h
@@ -0,0 +1,19 @@
+/* -*- C -*-
+ *
+ *                       Julian C. Cummings
+ *                California Institute of Technology
+ *                   (C) 2004 All Rights Reserved
+ *
+ */
+
+#if !defined(__config_portinfo_h__)
+#define __config_portinfo_h__
+
+#include <config_platform.h>
+#include <config_compiler.h>
+
+#include "config_local.h"
+
+#endif
+
+/* End of file */
