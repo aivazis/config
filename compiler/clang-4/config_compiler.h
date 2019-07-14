d543a48803c99073b2a30684c3c4c73fae08490d
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-07-15 02:14:48 +0300

    compiler: added support for {clang-4} to make {conda} happy

---
+++ compiler/clang-4/config_compiler.h
@@ -0,0 +1,18 @@
+/*
+ * -*- C -*-
+ *
+ * michael a.g. aïvázis
+ * parasim
+ * (c) 1998-2018  all rights reserved
+ *
+ */
+
+#if !defined(__config_compiler_h__)
+#define __config_compiler_h__
+
+/* The gcc preprocessor defines __FUNC__ */
+#define HAVE__FUNC__
+
+#endif
+
+/* end of file */
